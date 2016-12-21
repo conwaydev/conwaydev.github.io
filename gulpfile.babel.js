2bab0092a83e7903ab00bac8e6281ff9d7160465
Justin Conway <justin@conwaydev.com>
2016-12-20 19:40:14 -0600

    More build set up

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,73 +1,92 @@
 'use strict';
 
-import autoprefixer from 'autoprefixer';
-import child from 'child_process';
-import cssnano from 'cssnano';
-import gulp from 'gulp';
-import gutil from 'gulp-util';
-import postcss from 'gulp-postcss';
-import sass from 'gulp-sass';
-import svgSprite from 'gulp-svg-sprite';
-
+const child = require('child_process');
 const browserSync = require('browser-sync').create();
 
-const cssDir = './_scss/**/*.scss';
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import svgSprite from 'gulp-svg-sprite';
 
-gulp.task('jekyll', ['sass'], () => {
-    const jekyll = child.spawn('jekyll', ['build']);
+const siteRoot = '_site';
+const cssFiles = '_scss/**/*.scss';
 
-    const jekyllLogger = (buffer) => {
-        buffer.toString()
-            .split(/\n/)
-            .forEach((message) => gutil.log('Jekyll: ' + message));
-    };
+gulp.task('jekyll', () => {
+	const jekyll = child.spawn('jekyll', ['build',
+		'--watch',
+		'--incremental',
+		'--drafts'
+	]);
 
-    jekyll.stdout.on('data', jekyllLogger);
-    jekyll.stderr.on('data', jekyllLogger);
+	const jekyllLogger = (buffer) => {
+		buffer.toString()
+			.split(/\n/)
+			.forEach((message) => gutil.log('Jekyll: ' + message));
+	};
+
+	jekyll.stdout.on('data', jekyllLogger);
+	jekyll.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('scss', ()=> {
+	const processors = [
+		autoprefixer({browsers: ['last 2 versions']}),
+		cssnano(),
+	];
+
+	return gulp.src(cssFiles)
+		.pipe(sass())
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('./_includes/css'));
 });
 
 gulp.task('svg', () => {
-    gulp.src('./assets/svg/*.svg')
-        .pipe(svgSprite({
-            shape: {
-                dimension: {
-                    maxWidth : 40,
-                    maxHeight: 40
-                },
-                spacing : {
-                    padding: 10
-                },
-                dest: 'intermediate-svg'
-            },
-            mode : {
-                symbol: true
-            }
-        }))
-        .pipe(gulp.dest('./_includes/svg'));
+	gulp.src('./assets/svg/*.svg')
+		.pipe(svgSprite({
+			shape: {
+				dimension: {
+					maxWidth : 40,
+					maxHeight: 40
+				},
+				spacing : {
+					padding: 10
+				},
+				dest: 'intermediate-svg'
+			},
+			mode : {
+				symbol: true
+			}
+		}))
+		.pipe(gulp.dest('./_includes/svg'));
 });
 
-gulp.task('sass', () => {
-    const processors = [
-        autoprefixer({browsers: ['last 2 versions']}),
-        cssnano(),
-    ];
+gulp.task('webpack', () => {
+	const webpack = child.exec('webpack --config webpack.production.config.js -p');
 
-    return gulp.src(cssDir)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('./_includes/css'))
-        .pipe(browserSync.stream());
+	const webpackLogger = (buffer) => {
+		buffer.toString()
+			.split(/\n/)
+			.forEach((message) => gutil.log('Webpack: ' + message));
+	};
+
+	webpack.stdout.on('data', webpackLogger);
+	webpack.stderr.on('data', webpackLogger);
 });
 
+gulp.task('serve', () => {
+	browserSync.init({
+		files: [siteRoot + '/**'],
+		port: 4000,
+		server: {
+			baseDir: siteRoot
+		}
+	});
 
-// Static server
-gulp.task('serve', ['sass'], () => {
-    browserSync.init({
-        server: {
-            baseDir: '_site'
-        }
-    });
-    gulp.watch('./**/*.html').on('change', ['jekyll']);
+	gulp.watch('./_scss/**/*.scss', ['scss']);
+	gulp.watch('./scripts/**/*.js', ['webpack']);
 });
 
-gulp.task('default', ['svg', 'sass', 'jekyll', 'serve']);
+gulp.task('default', ['svg', 'scss', 'jekyll', 'serve']);
