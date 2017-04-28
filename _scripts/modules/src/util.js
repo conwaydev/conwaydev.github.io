9b8a4ee330e2403af11f85bd22e75b5b379089b4
Justin Conway <justin@conwaydev.com>
2017-04-27 22:37:47 -0500

    Wrap webfont loading in a doc ready

---
+++ _scripts/modules/src/util.js
@@ -0,0 +1,11 @@
+export function ready(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+export default {
+    ready
+}
--- _scripts/modules/src/webfont.js
+++ _scripts/modules/src/webfont.js
@@ -1,10 +1,13 @@
 import WebFont from 'webfontloader';
+import { ready } from 'util';
 
-WebFont.load({
-    google: {
-        families: [
-            'Poppins:600',
-            'Roboto Mono:400,700'
-        ]
-    }
+ready(()=> {
+    WebFont.load({
+        google: {
+            families: [
+                'Poppins:600',
+                'Roboto Mono:400,700'
+            ]
+        }
+    });
 });
