13181eb6757cf4a802439aacd041692ecfb89568
Justin Conway <justin@conwaydev.com>
2018-12-26 14:16:45 -0600

    feat: makeover
    BREAKING CHANGES: redo the entire site with tailwind

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const child = require('child_process');
 const browserSync = require('browser-sync').create();
 
 const gulp = require('gulp');
-const gutil = require('gulp-util');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -13,23 +11,6 @@ const svgSprite = require('gulp-svg-sprite');
 
 const siteRoot = '_site';
 const cssFiles = '_scss/**/*.scss';
-
-gulp.task('jekyll', () => {
-	const jekyll = child.spawn('jekyll', ['build',
-		'--watch',
-		'--incremental',
-		'--drafts'
-	]);
-
-	const jekyllLogger = (buffer) => {
-		buffer.toString()
-			.split(/\n/)
-			.forEach((message) => gutil.log('Jekyll: ' + message));
-	};
-
-	jekyll.stdout.on('data', jekyllLogger);
-	jekyll.stderr.on('data', jekyllLogger);
-});
 
 gulp.task('scss', ()=> {
 	const processors = [
@@ -61,18 +42,6 @@ gulp.task('svg', () => {
 			}
 		}))
 		.pipe(gulp.dest('./_includes/svg'));
-});
-
-gulp.task('serve', () => {
-	browserSync.init({
-		files: [siteRoot + '/**'],
-		port: 4000,
-		server: {
-			baseDir: siteRoot
-		}
-	});
-
-	gulp.watch('./_scss/**/*.scss', ['scss']);
 });
 
 gulp.task('default', ['svg', 'scss']);
