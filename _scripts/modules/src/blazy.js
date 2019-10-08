ac284ec909a4edd00e753786f9a1060aebb6aa5f
Justin Conway <justin@conwaydev.com>
2019-10-07 23:34:28 -0500

    fix: use native lazy loading

--- _scripts/modules/src/blazy.js
+++ _scripts/modules/src/blazy.js
@@ -1,6 +1,19 @@
-import Blazy from 'blazy';
 import { ready } from './util';
 
 ready(() => {
-    new Blazy();
+  if ('loading' in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll("img");
+    images.forEach(img => {
+      if (img.dataset.src) {
+        img.src = img.dataset.src;
+      }
+    });
+  } else {
+    // Dynamically import the LazySizes library
+    let script = document.createElement("script");
+    script.async = true;
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+    document.body.appendChild(script);
+  }
 });
--- _scripts/modules/src/util.js
+++ _scripts/modules/src/util.js
@@ -5,17 +5,3 @@ export function ready(fn) {
         document.addEventListener('DOMContentLoaded', fn);
     }
 }
-
-export function throttle(callback, limit) {
-    let wait = false;
-
-    return function() {
-        if (!wait) {
-            callback.apply(null, arguments);
-            wait = true;
-            setTimeout(function() {
-                wait = false;
-            }, limit);
-        }
-    };
-}
