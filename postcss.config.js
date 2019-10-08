063cb7425a20ac79522cd11a23b09ddad22edc04
Justin Conway <justin@conwaydev.com>
2019-10-07 23:50:11 -0500

    fix: fix more things

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
+const postcss = require('postcss-uncss');
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    require('postcss-uncss')({
+      html: ['_site/index.html', '_site/blog/**/*.html'],
+    }),
     require('cssnano')
   ]
 };
