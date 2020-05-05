84e709b54347fbe4af27ba2de9d686a35c3db6e3
Justin Conway <justin@conwaydev.com>
2020-05-04 20:31:06 -0500

    update postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,10 @@
-const postcss = require('postcss-uncss');
-
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
     require('cssnano'),
     require('postcss-uncss')({
-      html: ['_site/index.html', '_site/blog/**/*.html'],
+      html: ['_site/**/*.html'],
     })
   ]
 };
