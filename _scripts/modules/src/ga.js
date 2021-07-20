a1f7ef966a217ce5946ce752b589435870dd668f
Justin Conway <justin@conwaydev.com>
2021-07-20 12:41:12 -0500

    chore: update ga

--- _scripts/modules/src/ga.js
+++ _scripts/modules/src/ga.js
@@ -1,7 +1,10 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-38274252-1', 'auto');
-ga('send', 'pageview');
+const script = document.createElement("script");
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-1XCBW4CS93";
+script.async = true;
+script.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-1XCBW4CS93');
+};
+document.body.appendChild(script);
