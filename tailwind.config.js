577520260762452b7c9ad8652115e45d6d2638cd
Justin Conway <justin@conwaydev.com>
2023-03-07 12:38:57 -0600

    chore: update for the code block

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  content: [
+    './_includes/*.{html,js}',
+    './_layouts/*.{html,js}',
+    './_posts/*.{html,md}',
+    './index.html',
+    './uses.markdown',
+  ],
   theme: {
     maxWidth: {
       '7xl': '92rem',
